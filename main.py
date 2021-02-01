104bfbe6a9dae8f97e34c0755c568733f91c254f
JamesObry <59664773+JamesObry@users.noreply.github.com>
2021-02-02 01:26:56 +0300

    Added simple system and connected sqlite3
    Added simple system and connected sqlite3

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import telebot
 import random
-from bs4 import BeautifulSoup
+import sqlite3 as sq
 from telebot import types
 
 bot = telebot.TeleBot('1511767750:AAFygVIbM1AyISAbOJ0PiAypuXmMSTQMZjE')
 
 gameVariants = [1, 2, 3]
+
+
+# data base
+with sq.connect("users.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+        telegram_id INTEGER PRIMARY KEY,
+        name TEXT,
+        balance INTEGER,
+        rank INTEGER
+    )""")
+
 
 # keyboards
 mainBoard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
@@ -26,11 +38,19 @@ gameBoard.add(gameStone, gameScissors, gamePaper, gameRandom, gameBack)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'hello', reply_markup=mainBoard)
+    try:
+        with sq.connect("users.db") as con:
+            cur = con.cursor()
+            cur.execute(f"INSERT INTO users(telegram_id, name, balance, rank) VALUES({message.chat.id}, 'Не указано', 5000, 'Новичок')")
+    except:
+        pass
 
+# main navigation
 # game command
 @bot.message_handler(regexp='Играть')
 def playGame(message):
     bot.send_message(message.chat.id, 'выберите кнопку', reply_markup=gameBoard)
+# game commands
 @bot.message_handler(regexp='Камень')
 def gamePlayStone(message):
     botChoice = random.choice(gameVariants)
@@ -71,6 +91,17 @@ def gamePlayRandom(message):
         bot.send_message(message.chat.id, '-')
     elif botChoice < playerChoice:
         bot.send_message(message.chat.id, '+')
-
+# main | CheckMyStats
+@bot.message_handler(regexp='Мой профиль')
+def checkMyStats(message):
+    with sq.connect("users.db") as con:
+        cur = con.cursor()
+        name = cur.execute(f"SELECT name FROM users WHERE telegram_id = {message.chat.id}")
+        name = cur.fetchone()[0]
+        balance = cur.execute(f"SELECT balance FROM users WHERE telegram_id = {message.chat.id}")
+        balance = cur.fetchone()[0]
+        rank = cur.execute(f"SELECT rank FROM users WHERE telegram_id = {message.chat.id}")
+        rank = cur.fetchone()[0]
+    bot.send_message(message.chat.id, f'Профиль:\n\nИмя: {name}\nБаланс: {balance}\nСкилл: {rank}')
 
 bot.infinity_polling(True)
